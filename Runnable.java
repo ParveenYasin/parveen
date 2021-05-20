1f030887e2e71145d10e6ea6d13ce29b5417f8e2
ParveenYasin <83947176+ParveenYasin@users.noreply.github.com>
2021-05-20 15:58:41 +0530

    Add files via upload

---
+++ Runnable.java
@@ -0,0 +1,28 @@
+package multithread;
+
+public class Runnable extends Thread{
+	public void run() {
+			for(int i=0;i<=4;i++) {
+				System.out.println(i);
+				try {
+					sleep(1000);
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	class Deer{
+		public void main(String[] args) {
+			Runnable r=new Runnable();
+			r.start();
+			System.err.println("Before setting name"+Thread.currentThread().getName());
+			Thread.currentThread().setName("load");
+			System.out.println("after setting name"+Thread.currentThread().getName());
+			System.out.println("before setting priority"+Thread.currentThread().getPriority());
+			Thread.currentThread().setPriority(3);
+			System.out.println("After setting priority"+Thread.currentThread().getPriority());
+		}
+	}
+	}
+
