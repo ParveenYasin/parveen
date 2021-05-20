483734900e1af6dd3ca83ec43c500cd3426c90bc
ParveenYasin <83947176+ParveenYasin@users.noreply.github.com>
2021-05-20 15:38:09 +0530

    Add files via upload

---
+++ HashSetEx.java
@@ -0,0 +1,31 @@
+package collection;
+import java.util.*;
+public class HashSetEx 
+{
+	static Set<Integer> num=new HashSet<Integer>();
+	static public Set CountryNames()
+	{
+		Set<String> mylist=new HashSet<String>();
+		mylist.add("india");
+		mylist.add("america");
+		mylist.add("newzeland");
+		mylist.add("Greenland");
+		mylist.add("europe");
+		return mylist;
+	}
+	static public Set modify()
+	{
+	 for(int i=11;i<=15;i++)
+	  num.add(i);
+	  return num;
+	}
+	static public Set numbers()
+	{
+		for(int i=1;i<10;i++)num.add(i);
+		return num;
+	}	
+}
+
+
+
+
