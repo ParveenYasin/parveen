ae992700b234b48850389c5e4cc0cd86e4206eb3
ParveenYasin <83947176+ParveenYasin@users.noreply.github.com>
2021-05-21 22:56:28 +0530

    Add files via upload

---
+++ Problem.java
@@ -0,0 +1,10 @@
+
+public class Problem {
+	public static void main(String[] args) {
+		MainList m=new MainList();
+		System.out.println(m.storeEvenNumbers(48));
+		System.out.println(m.storeSquareEvenNumbers());
+		System.out.println(m.find(44));
+	}
+
+}
