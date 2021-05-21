1d7bb626cb01341210d6ec9f3d1e5bec99acff75
ParveenYasin <83947176+ParveenYasin@users.noreply.github.com>
2021-05-21 22:56:41 +0530

    Add files via upload

---
+++ problem1.java
@@ -0,0 +1,10 @@
+
+public class problem1 {
+	public static void main(String[] args) {
+		MainSet m1=new MainSet();
+		m1.storeCountryName("India");
+		m1.storeCountryName("pakis");
+		System.out.println("India");
+	}
+
+}
