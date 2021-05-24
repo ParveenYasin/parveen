85da630809f6beb512910dc7af782db5823b6733
ParveenYasin <83947176+ParveenYasin@users.noreply.github.com>
2021-05-24 09:24:26 -0700

    Add files via upload

---
+++ FileReverseReading.java
@@ -0,0 +1,21 @@
+package sample;
+import java.io.*;
+public class FileReverseReading 
+{
+	public static void main (String [] args)throws IOException
+	{
+		FileReader fr = new FileReader("d:/Data.txt");
+		BufferedReader br = new BufferedReader(fr);
+
+		String data;  
+        
+		while ((data = br.readLine()) != null){
+		    String[] words = data.split(" ");
+		    for(String a: words)
+		    {
+		        StringBuilder builder=new StringBuilder(a);
+		        System.out.print(builder.reverse().toString()+" ");
+		    }  
+		}
+	}
+}
